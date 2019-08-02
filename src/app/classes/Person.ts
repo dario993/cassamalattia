e98b59ea78d67df3253f34b93c271f745ee8790d
dario993 <52817568+dario993@users.noreply.github.com>
2019-08-02 13:38:33 +0200

    update from web, select franchigia

--- src/app/classes/Person.ts
+++ src/app/classes/Person.ts
@@ -1,6 +1,10 @@
 import { PersonInterface } from '../interfaces/person';
 
-const _franchigie = ['100', '200', '300'];
+const _franchigie = [ 
+  { id: "1", value: "100", selected: 'false' }, 
+  { id: "2", value: "200", selected: 'false' }, 
+  { id: "3", value: "300", selected: 'true' } 
+  ];
 
 export class Person implements PersonInterface{
   
